2b7929f342f5c82218084b3268d4d44df8c91a57
GilangPambudi <2131710126@student.polinema.ac.id>
2025-04-15 12:18:35 +0700

    Remove couple routes and associated controller references

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\GuestController;
-use App\Http\Controllers\CoupleController;
 use App\Http\Controllers\QRCodeController;
 use App\Http\Controllers\InvitationController;
-use App\Http\Controllers\WeddingEventController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,18 +18,6 @@ Route::get('/home', [HomeController::class, 'index']);
 Route::group(['prefix' => 'qr'], function () {
     Route::get('/', [QRCodeController::class, 'index']);
     Route::post('/submit', [QRCodeController::class, 'submit']);
-});
-
-Route::group(['prefix' => 'couple', 'middleware' => 'auth'], function () {
-    Route::get('/', [CoupleController::class, 'index']);
-    Route::post('/list', [CoupleController::class, 'list']);
-    Route::get('/create_ajax', [CoupleController::class, 'create_ajax']);
-    Route::post('/store_ajax', [CoupleController::class, 'store_ajax']);
-    Route::get('/{id}/show_ajax', [CoupleController::class, 'show_ajax']);
-    Route::get('/{id}/edit_ajax', [CoupleController::class, 'edit_ajax']);
-    Route::put('/{id}/update_ajax', [CoupleController::class, 'update_ajax']);
-    Route::get('/{id}/delete_ajax', [CoupleController::class, 'confirm_ajax']);
-    Route::delete('/{id}/delete_ajax', [CoupleController::class, 'delete_ajax']);
 });
 
 Route::group(['prefix' => 'guests', 'middleware' => 'auth'], function () {
